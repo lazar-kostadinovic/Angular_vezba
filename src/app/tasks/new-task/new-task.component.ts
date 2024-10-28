556208073a2353a353c1362168d55d1f03b29244
Lazar Kostadinovic <kostadinovicl999@gmail.com>
2024-10-28 21:52:01 +0100

    Finished app

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {NewTaskData} from '../task/task.model'
+import { NewTaskData } from '../task/task.model'
+import { TaskService } from '../task.service';
 
 
 @Component({
@@ -11,8 +12,9 @@ import {NewTaskData} from '../task/task.model'
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({required:true}) userId!: string;
+  @Output() close = new EventEmitter<void>();
+  //@Output() add = new EventEmitter<NewTaskData>();
   enteredTitle= '';
   enteredSummary='';
   enteredDate='';
@@ -21,16 +23,23 @@ export class NewTaskComponent {
 //   enteredTitle = signal('');
 //   enteredSummary = signal('');
 //   enteredDate = signal('');
-
+  private taskService = inject(TaskService);
 
   onCloseButton(){
-    this.cancel.emit()
+    this.close.emit()
   }
+  
   onSubimt(){
-     this.add.emit({
-      title: this.enteredTitle,
-      summary: this.enteredSummary,
-      dueDate: this.enteredDate
-     })
+    //  this.add.emit({
+    //   title: this.enteredTitle,
+    //   summary: this.enteredSummary,
+    //   dueDate: this.enteredDate
+    //  })
+    this.taskService.addTask({
+        title: this.enteredTitle,
+        summary: this.enteredSummary,
+        dueDate: this.enteredDate
+    },this.userId);
+    this.close.emit();
   }
 } 
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
 import { NewTaskData, Task } from './task/task.model';
-import { CardComponent } from "../shared/card/card.component";
+import { TaskService } from './task.service';
+
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent, NewTaskComponent, CardComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -17,58 +17,28 @@ export class TasksComponent {
   @Input({required:true}) id!:string;
   @Input({required:true}) name!:string | undefined;
   isAddingTaks=false;
-   dummyTasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
-
+  // private taskService= new TaskService(); umesto ovog pristupa koristimo dependency injection da ne bi u svakoj komponenti kreirali novu instancu klase nego da kreiramo jednu preko konstruktora
+ 
+  // private taskService: TaskService;
+  // constructor(taskService: TaskService){
+  //   this.taskService=taskService;
+  // }
+  constructor(private taskService: TaskService){}
+  
   get selectedUserTasks(){
-    return this.dummyTasks.filter((task)=> task.userId === this.id)
+    return this.taskService.getUserTaks(this.id);
   }
 
-  onCompleteTask(id:string) {
-    this.dummyTasks = this.dummyTasks.filter((task)=> task.id!==id);
-  }
+  // onCompleteTask(id:string) {
+  //  this.taskService.removeTask(id);
+  // }
 
   onStartAddTask(){
     this.isAddingTaks=true;
-    this.dummyTasks.push();
+    //this.taskService.addTaks();
   }
 
   onCloseButton(){
-    this.isAddingTaks=false;
-  }
-
-  onSubmit(taskData: NewTaskData){
-    let id='u4';
-    this.dummyTasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.id,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.dueDate
-    });
     this.isAddingTaks=false;
   }
 
